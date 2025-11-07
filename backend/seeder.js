a9d5eba3c8229f7b4cf571514b5f843ab4fc187e
danirso <danielsrossi43@gmail.com>
2025-11-07 11:09:53 -0300

    inserting docker into the project

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,7 +1,5 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 async function seed() {
   try {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
